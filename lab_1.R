7aca5691daba8a86720580404760a6cb9fbc3646
Jake-Kistler <jakistler01@gmail.com>
2025-08-31 18:49:05 -0500

    Start of task 3, getting the # of species in the data set

--- lab_1.R
+++ lab_1.R
@@ -1,7 +1,7 @@
-# Task 1
+# Task 1:
 getwd()
 
-# Task 2
+# Task 2:
 # using some of the given example R code we can read data into my instance of R studio
 # Although at this time I'm not sure of what type of data data( the var) is here
 # maybe a vector?
@@ -9,3 +9,9 @@ data <- read.csv("DDT-1.csv")
 
 # Apply more given code, this gives me an entire row of the .csv file. 2nd arg is number of rows to be returned
 head(data,6)
+
+# Task 3:
+# This task asks about some metadata about the data object we used above
+
+# Number of species in the data set
+length(unique(data$SPECIES))
