d8159f8097dcafb8a611a046bad4ee7b40766097
Jake-Kistler <jakistler01@gmail.com>
2025-08-31 16:02:34 -0500

    Task 2 is now done.

--- lab_1.R
+++ lab_1.R
@@ -2,3 +2,10 @@
 getwd()
 
 # Task 2
+# using some of the given example R code we can read data into my instance of R studio
+# Although at this time I'm not sure of what type of data data( the var) is here
+# maybe a vector?
+data <- read.csv("DDT-1.csv")
+
+# Apply more given code, this gives me an entire row of the .csv file. 2nd arg is number of rows to be returned
+head(data,6)
